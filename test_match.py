d42e7ad44c1324295e16a7787d65b3c5417e15f8
Neil Lakin <Neil@dhcp-ccc-421.redrover.cornell.edu>
2015-05-11 23:08:16 -0400

    done for the day

--- test_match.py
+++ test_match.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-MIN_MATCHES=5
+MIN_MATCHES=6
 
 def drawMatches(template, image, kp_t, kp_i, matches):
     result = np.zeros(shape =(max([template.shape[0],image.shape[0]]), template.shape[1]+image.shape[1],3), dtype=np.uint8)
@@ -23,6 +23,7 @@ def findMatches(template, image):
     Takes grayscale template and image, finds SIFT matches, draws a box around them.
     """
     sift = cv2.SIFT()
+    #sift = cv2.SURF()
     kp_t, des_t = sift.detectAndCompute(template,None)
     kp_i, des_i = sift.detectAndCompute(image,None)
 
@@ -34,7 +35,7 @@ def findMatches(template, image):
 
     good = []
     for m,n in matches:
-        if m.distance < .6*n.distance:
+        if m.distance < .5*n.distance:
             good.append(m)
 
     return good, kp_t, kp_i
@@ -54,15 +55,14 @@ def findObjects(template, image, kp_t, kp_i, matches):
     corners = np.float32([[0,0],[0,h-1],[w-1,h-1],[w-1,0]]).reshape(-1,1,2)
 
     while(n_points >= MIN_MATCHES):
-        print "Shape: " + str(obj_points.shape)
         if obj_points.shape[0] < MIN_MATCHES:
             break
-        H, mask = cv2.findHomography(obj_points, scene_points, cv2.RANSAC, ransacReprojThreshold=5)
+        H, mask = cv2.findHomography(obj_points, scene_points, cv2.RANSAC, ransacReprojThreshold=3)
+        print H
         n_points = np.count_nonzero(mask)
         if n_points < MIN_MATCHES:
             break
         object = cv2.perspectiveTransform(corners, H).astype(np.int)
-        print object.shape
         #cv2.polylines(result, object, True, (0,255,0), 4, lineType=cv2.CV_AA)
         cv2.line(result, (object[0][0][0],object[0][0][1]),(object[1][0][0],object[1][0][1]), (0,255,0), thickness=4)
         cv2.line(result, (object[1][0][0],object[1][0][1]),(object[2][0][0],object[2][0][1]), (0,255,0), thickness=4)
@@ -71,11 +71,13 @@ def findObjects(template, image, kp_t, kp_i, matches):
         indices = np.where(mask==0)[0]
         obj_points = obj_points[indices]
         scene_points = scene_points[indices]
+        plt.imshow(result)
+        plt.show()
 
-    plt.imshow(result)
-    plt.show()
+    #plt.imshow(result)
+    #plt.show()
 
-    return obj_points,scene_points
+    return result
 
 
 
@@ -101,3 +103,18 @@ def drawObject(template, image, good):
     cv2.rectangle(result,tuple([min_x,min_y]),tuple([max_x,max_y]), color, thickness=5)
     plt.imshow(result)
     plt.show()
+
+if __name__ == "__main__":
+    plt.gray()
+
+    #stub = 'superman/'
+    stub = ''
+    template = cv2.imread(stub + 'template.jpg',0)
+    for i in range(1,10):
+        im = cv2.imread(stub + 'image' + str(i) + '.jpg', 0)
+        matches, kpt, kpi = findMatches(template, im)
+        result = findObjects(template, im, kpt, kpi, matches)
+        fig, ax = plt.subplots(1,1)
+        ax.imshow(result)
+        fig.show()
+    input('enter to exit.')
