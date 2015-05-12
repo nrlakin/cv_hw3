e9813f55ce13ecb5c4d0842af65c6fa0751f6314
Brandon <baplaste@gmail.com>
2015-05-12 15:58:36 -0400

    adding scaling

--- quantize.py
+++ quantize.py
@@ -64,6 +64,12 @@ def quantizeImage(image, clusters, cluster_threshold):
 	img_qnt = img_qnt.reshape((h, w, 3))
 	return cv2.cvtColor(img_qnt, cv2.COLOR_LAB2BGR)
 
+def loadImage(path, max_width):
+	image = cv2.imread(path)
+	if image.shape[1] != max_width:
+		scale = max_width/image.shape[1]
+		image = cv2.resize(image, (0,0), fx=scale, fy=scale)
+	return image
 
 # Main
 if __name__ == '__main__':
@@ -71,37 +77,31 @@ if __name__ == '__main__':
 	# Variables
 	saturation = 30
 	max_template_width = 400.0
-	max_image_width = 800.0
+	max_image_width = 1000.0
 	cluster_threshold = 90
 	cluster_count = 4
 
 	# Load images (resize template if necessary)
-	template = cv2.imread('Superman/template.png')
-	if template.shape[1] > max_template_width:
-		scale = max_template_width/template.shape[1]
-		template = cv2.resize(template, (0,0), fx=scale, fy=scale)
-	img_bgr = cv2.imread('Superman/image5.jpg')
-	if img_bgr.shape[1] > max_image_width:
-		scale = max_image_width/img_bgr.shape[1]
-		img_bgr = cv2.resize(img_bgr, (0,0), fx=scale, fy=scale)
+	template = loadImage('roadsign/template.jpg', max_template_width)
+	img_bgr = loadImage('roadsign/image11.jpg', max_image_width)
 
 	# Get color palette
 	template_qnt = saturateImage(template, saturation)
-
 	template_qnt, clusters = calculateQuantizedPalette(template_qnt, cluster_count)
-	template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-	template_qnt_gray = cv2.cvtColor(template_qnt, cv2.COLOR_BGR2GRAY)
-
 	cv2.imshow("image1", np.hstack([template, template_qnt]))
 
 	# # Color Quantization
 	# (h, w) = img_bgr.shape[:2]
 	# img_qnt = saturateImage(img_bgr, saturation)
+	# img_qnt = cv2.bilateralFilter(img_qnt,5,50,50)
+
 	# img_qnt = quantizeImage(img_qnt, clusters, cluster_threshold)
 
 	# cv2.imshow("image2", np.hstack([img_bgr, img_qnt]))
 	# # cv2.waitKey(0)
 
+	# template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+	template_qnt_gray = cv2.cvtColor(template_qnt, cv2.COLOR_BGR2GRAY)
 	# img_qnt_gray = cv2.cvtColor(img_qnt, cv2.COLOR_BGR2GRAY)
 	img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
